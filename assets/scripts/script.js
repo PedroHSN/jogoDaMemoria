a4658d16465e4d2d107b82a4c3e9979f666a9b5d
pedrohsn <pedrodti2017@gmail.com>
2022-03-27 23:54:37 -0300

    criando as cartas do jogo com javascript

---
+++ assets/scripts/script.js
@@ -0,0 +1,43 @@
+const FRONT = "card_front";
+const BACK = "card_back"
+
+let techs = ['bootstrap',
+'css',
+'electron',
+'html',
+'firebase',
+'javascript',
+'jquery',
+'mongo',
+'node',
+'react'];
+
+createCardsFromTechs(techs);
+
+function createCardsFromTechs(techs) {
+
+  let cards = [];
+
+  for(let tech of techs){
+    cards.push(createPairFromTech(tech));
+  }
+
+  console.log(cards.flatMap(pair => pair));
+}
+
+function createPairFromTech(tech){
+
+    return [{
+        id: createIdWidthTech(tech),
+        icon: tech,
+        flipped: false,
+    }, {
+        id: createIdWidthTech(tech),
+        icon: tech,
+        flipped: false,
+    }]
+}
+
+function createIdWidthTech(tech){
+  return tech + parseInt(Math.random() * 1000)
+}
